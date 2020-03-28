dd26a8e77229ff6136dfc9f2a810b56e60dab736
DevinBayly <baylyd@email.arizona.edu>
2020-03-28 02:40:33 +0000

    xxxx

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 // testing out a first git change
+
+// doing stuff with structs now
+
+struct container {
+    el:String
+}
+
 fn main() {
+    let s = container{
+        el:String::new()
+    };
+
     println!("Hello, Marcos!!");
 }
